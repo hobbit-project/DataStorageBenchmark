70bccc2e91407496edb2af3c183aacac1f3299bf
Mirko Spasic <mirkospasic@gmail.com>
2018-01-18 17:07:04 +0100

    Do not create text index

--- system/load.sh
+++ system/load.sh
@@ -7,6 +7,7 @@ GRAPHURI=$4
 
 sleep 1
 echo "delete from load_list;" | isql $ADDRESS:$PORT
+isql $ADDRESS:$PORT exec="DB.DBA.RDF_OBJ_FT_RULE_DEL (null, null, 'ALL');;"
 echo "ld_dir('"$FOLDER"', '*', '"$GRAPHURI"');" | isql $ADDRESS:$PORT
 for i in `seq 1 $NUMBEROFLOADERS`;
 do
