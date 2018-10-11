90654fb7480af0329e433b55ada88c5b902b9c67
Mirko Spasic <mirkospasic@gmail.com>
2018-10-11 14:41:17 +0200

    New system: Virtuoso 8.2

--- prepare_dockers.sh
+++ prepare_dockers.sh
@@ -7,6 +7,7 @@ docker build -f docker/virtuososystem.docker -t git.project-hobbit.eu:4567/mspas
 #docker build -f docker/virtuosocomsystem72.docker -t git.project-hobbit.eu:4567/mspasic/virtuosocomsystem72 .
 #docker build -f docker/virtuosocomsystem80.docker -t git.project-hobbit.eu:4567/mspasic/virtuosocomsystem80 .
 #docker build -f docker/virtuosocomsystem81.docker -t git.project-hobbit.eu:4567/mspasic/virtuosocomsystem81 .
+#docker build -f docker/virtuosocomsystem82.docker -t git.project-hobbit.eu:4567/mspasic/virtuosocomsystem82 .
 docker build -f docker/sparql-snbbenchmarkcontroller.docker -t git.project-hobbit.eu:4567/mspasic/sparql-snbbenchmarkcontroller .
 docker build -f docker/sparql-snbdatagenerator.docker -t git.project-hobbit.eu:4567/mspasic/sparql-snbdatagenerator .
 docker build -f docker/sparql-snbtaskgenerator.docker -t git.project-hobbit.eu:4567/mspasic/sparql-snbtaskgenerator .
@@ -17,6 +18,7 @@ docker push git.project-hobbit.eu:4567/mspasic/virtuososystem
 #docker push git.project-hobbit.eu:4567/mspasic/virtuosocomsystem72
 #docker push git.project-hobbit.eu:4567/mspasic/virtuosocomsystem80
 #docker push git.project-hobbit.eu:4567/mspasic/virtuosocomsystem81
+#docker push git.project-hobbit.eu:4567/mspasic/virtuosocomsystem82
 docker push git.project-hobbit.eu:4567/mspasic/sparql-snbbenchmarkcontroller
 docker push git.project-hobbit.eu:4567/mspasic/sparql-snbdatagenerator
 docker push git.project-hobbit.eu:4567/mspasic/sparql-snbtaskgenerator
